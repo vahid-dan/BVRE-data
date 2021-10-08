2d268d976a6d6d5c52aec9e372614ac336454609
abreefpilz <abreefpilz@vt.edu>
2021-10-08 15:24:32 -0400

    Updated the script with files and fixes

--- Combine_old_BVR_files.R
+++ Combine_old_BVR_files.R
@@ -8,9 +8,13 @@ library(lubridate)
 library(dplyr)
 
 #to download the files needed
-#download.file("https://github.com/CareyLabVT/ManualDownloadsSCCData/master/BVRplatform_manual_2020.csv", "BVRplatform_manual_2020.csv")
-#download.file('https://github.com/FLARE-forecast/BVRE-data/bvre-platform-data/bvre-waterquality.csv', "bvre-waterquality.csv") 
-#download.file('https://github.com/FLARE-forecast/BVRE-data/bvre-platform-data/BVRplatform.csv.csv', "BVRplatform.csv.csv")
+download.file("https://github.com/CareyLabVT/ManualDownloadsSCCData/master/BVRplatform_manual_2020.csv", "BVRplatform_manual_2020.csv")
+download.file('https://github.com/FLARE-forecast/BVRE-data/raw/bvre-platform-data/bvre-waterquality.csv', "bvre-waterquality.csv") 
+download.file('https://github.com/FLARE-forecast/BVRE-data/raw/bvre-platform-data/BVRplatform.csv', "BVRplatform.csv.csv")
+download.file("https://raw.githubusercontent.com/CareyLabVT/ManualDownloadsSCCData/master/BVRPlatform/CR6Series_BVRplatform_bvre-waterquality_20210531.dat", "CR6Series_BVRplatform_bvre-waterquality_20210531.dat" )
+download.file("https://raw.githubusercontent.com/CareyLabVT/ManualDownloadsSCCData/master/BVRPlatform/CR6Series_BVRplatform_bvre-waterquality_20210726.dat", "CR6Series_BVRplatform_bvre-waterquality_20210726.dat" )
+download.file("https://raw.githubusercontent.com/CareyLabVT/ManualDownloadsSCCData/master/BVRPlatform/CR6Series_BVRplatform_BVRplatform_20210416.dat", "CR6Series_BVRplatform_BVRplatform_20210416.dat")
+download.file("https://raw.githubusercontent.com/CareyLabVT/ManualDownloadsSCCData/master/BVRPlatform/CR6Series_BVRplatform_BVRplatform_20210610.dat", "CR6Series_BVRplatform_BVRplatform_20210610.dat" )
 
 bvrheader1<-read.csv("BVRplatform.csv", skip=1, as.is=T) #get header minus wonky Campbell rows
 bvrdata1<-read.csv("BVRplatform.csv", skip=4, header=F) #get data minus wonky Campbell rows
@@ -30,19 +34,23 @@ bvrdata= bvrd%>%
   distinct(TIMESTAMP, .keep_all= TRUE) #taking out the duplicate values 
    
 
-mydir = "Manual_Downloads"
+mydir = c("CR6Series_BVRplatform_bvre-waterquality_20210531.dat",
+          "CR6Series_BVRplatform_bvre-waterquality_20210726.dat",
+          "CR6Series_BVRplatform_BVRplatform_20210416.dat",
+          "CR6Series_BVRplatform_BVRplatform_20210610.dat")
+
 myfiles = list.files(path=mydir, pattern="", full.names=TRUE)#list the files from BVR platform
 
 #taking out the the Temp Test files
-myfilesBVR <- myfiles[ !grepl("CR6Series_BVRplatform_bvre-waterquality_20210416*", myfiles) ]#exclude the Temp test data
+#myfilesBVR <- myfiles[ !grepl("CR6Series_BVRplatform_bvre-waterquality_20210416*", myfiles) ]#exclude the Temp test data
 
 #create dataframe for the for loop
 bvrdata3<-""
 
 #combine all of the files into one data sheet, have to come back and fix this loop
-for(k in 1:length(myfilesBVR)){
-  bvrheader2<-read.csv(myfilesBVR[k], skip=1, as.is=T) #get header minus wonky Campbell rows
-  bvrdata2<-read.csv(myfilesBVR[k], skip=4, header=F) #get data minus wonky Campbell rows
+for(k in 1:length(myfiles)){
+  bvrheader2<-read.csv(myfiles[k], skip=1, as.is=T) #get header minus wonky Campbell rows
+  bvrdata2<-read.csv(myfiles[k], skip=4, header=F) #get data minus wonky Campbell rows
   names(bvrdata2)<-names(bvrheader2) #combine the names to deal with Campbell logger formatting
   bvrdata3=rbind(bvrdata2, bvrdata3)
 }
@@ -54,7 +62,7 @@ names(bvrdata5)<-names(bvrheader5)
 
 
 #bind all of the files with turbidity together
-bvrdata5=rbind(bvrdata3,bvrheader5)
+bvrdata5=rbind(bvrdata3,bvrdata5)
 
 bvrdata5= bvrdata5%>%
   rbind(.,bvrdata3)%>%
@@ -124,5 +132,5 @@ bvrdata_new=bvrdata_new%>%
   relocate(c(Turbidity_FNU_1,TSS_mgL_1), .after=fDOM_QSU_1)%>%
   filter(TIMESTAMP!="")
 
-
-write.csv(bvrdata_new, "bvre-waterquality.csv", row.names = F)
+#write as a new file 
+write.csv(bvrdata_new, "bvrewaterquality.csv", row.names = F)
